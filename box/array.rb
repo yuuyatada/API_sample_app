66f1c8c4bd7396ff9a3290b38d789dae2d299f4e
EC2 Default User <c134141@gmail.com>
2023-06-19 04:31:07 +0000

    else,elseifの使い方基礎

--- box/array.rb
+++ box/array.rb
@@ -44,3 +44,13 @@ end
 if score >= 50 || (score <= 100  && score >= 80)  # (score ≧ 50) or (80 ≦ score ≦ 100)
   puts "得点は50点以上、または80点以上100点以下です。"
 end
+
+apple = "Nagano"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
