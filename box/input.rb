80e2b42ba047a211aa3e29ab41d6ca28127cb025
EC2 Default User <c134141@gmail.com>
2023-06-19 04:58:23 +0000

    サイコロを用いてrand乱数の基礎

--- box/input.rb
+++ box/input.rb
@@ -1,3 +1,10 @@
-puts "キーボードから何か入力してみましょう"
-input_key = gets
-puts "入力された内容は#{input_key}"
+#puts "キーボードから何か入力してみましょう"
+#input_key = gets
+#puts "入力された内容は#{input_key}"
+
+dice = 0  # 変数diceに0を代入し、初期値を設定する
+
+while dice != 6 do  # サイコロの目が6ではない間、diceの初期値は0なので条件を満たす。以降はdiceに代入される数によって結果が異なる
+  dice = rand(1..6)  # 1～6の数字がランダムに出力される
+  puts dice
+end
