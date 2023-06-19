fb85e6b684832b9d70bf15847770a582489907a4
EC2 Default User <c134141@gmail.com>
2023-06-19 04:28:22 +0000

    3つ以上繋げた場合の処理方法の例

--- box/array.rb
+++ box/array.rb
@@ -33,3 +33,14 @@ end
 if (hand == "グー") || (hand == "パー") # この行を追加
   puts "出した手はグーまたはパーです" # この行を追加
 end # この行を追加
+
+
+score = 70
+
+if (score >= 50 || score <= 100) && score >= 80  # (score ≧ 50 or score ≦ 100) and (score ≧ 80)
+  puts "得点は50点以上または100点以下で、かつ80点以上です。"
+end
+
+if score >= 50 || (score <= 100  && score >= 80)  # (score ≧ 50) or (80 ≦ score ≦ 100)
+  puts "得点は50点以上、または80点以上100点以下です。"
+end
