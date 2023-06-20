23915bb1e9b895ebf23f1e5de452872279d74908
EC2 Default User <c134141@gmail.com>
2023-06-20 13:32:27 +0000

    breakを使った繰り返し処理中断の基礎

--- box/input.rb
+++ box/input.rb
@@ -13,7 +13,17 @@
  # puts i
 #end
 
-amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
-amounts.each do |fruit, amount|  #ハッシュの内容を順にキーをfruit、値をamountに代入して繰り返す
-  puts "#{fruit}は#{amount}個です。"
+#amounts = {"リンゴ"=>2, "イチゴ"=>5, "オレンジ"=>3}
+#amounts.each do |fruit, amount|  #ハッシュの内容を順にキーをfruit、値をamountに代入して繰り返す
+ # puts "#{fruit}は#{amount}個です。"
+#end
+
+i = 1
+while i <= 10 do
+  if i == 5
+    puts "処理を終了します"
+    break  # iが5になると繰り返しから抜ける
+  end
+  puts i
+  i += 1 # iの数値に1を加えたい時は、i = i +1と書く代わりに、i += 1と書くことができます。
 end
