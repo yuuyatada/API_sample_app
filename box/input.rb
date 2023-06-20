6c167d690db77292832ebe7be68abb3021ec4fde
EC2 Default User <c134141@gmail.com>
2023-06-20 13:45:16 +0000

    変数の処理を複数行うプログラムの作成

--- box/input.rb
+++ box/input.rb
@@ -29,12 +29,39 @@
 #end
 
 
+#puts "計算をはじめます"
+#puts "2つの値を入力してください"
+
+#a = gets.to_i
+#b = gets.to_i
+
+#puts "計算結果を出力します"
+#puts "#{a}*#{b}=#{a * b}"
+#puts "計算を終了します"
+
 puts "計算をはじめます"
-puts "2つの値を入力してください"
+puts "何回繰り返しますか？"
 
-a = gets.to_i
-b = gets.to_i
+input = gets.to_i
 
-puts "計算結果を出力します"
-puts "#{a}*#{b}=#{a * b}"
+i = 1
+while i <= input do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a + b}"
+  puts "#{a}-#{b}=#{a - b}"
+  puts "#{a}*#{b}=#{a * b}"
+  puts "#{a}/#{b}=#{a / b}"
+
+  i += 1
+end
+
 puts "計算を終了します"
