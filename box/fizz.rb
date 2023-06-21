8d209dca9352b37d9172c13e6bceb48a2ad7c543
EC2 Default User <c134141@gmail.com>
2023-06-21 14:34:22 +0000

    fizbazの基礎6

--- box/fizz.rb
+++ box/fizz.rb
@@ -1,6 +1,25 @@
-def greeting(name)
- return "Hello, #{name}!"
-   "Good morning, #{name}!"
+#def greeting(name)
+# return "Hello, #{name}!"
+#   "Good morning, #{name}!"
+#end
+
+#puts greeting('John')  # 'John'を引数として渡す
+
+def fizz_buzz(number)
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number.to_s
+  end
 end
 
-puts greeting('John')  # 'John'を引数として渡す
+puts "数字を入力してください。"
+
+input = gets.to_i
+
+puts '結果は...'
+puts fizz_buzz(input)
