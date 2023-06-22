9ee13923a7b147c382c80f98548f75e365b0428f
EC2 Default User <c134141@gmail.com>
2023-06-22 13:05:44 +0000

    オブジェクトのクラスについての基礎１

--- box/rails.rb
+++ box/rails.rb
@@ -1,16 +1,22 @@
-class Car
+#class Car
     
-    def turn(direction)
-        puts "#{direction}に曲がります。"
-    end
+   # def turn(direction)
+   #     puts "#{direction}に曲がります。"
+   # end
     
-  def run(distance)
-    puts "車で#{distance}キロ走ります。"
-  end
-  end
+  #def run(distance)
+  #  puts "車で#{distance}キロ走ります。"
+ # end
+  #end
   
-car = Car.new
-car.turn("右")
+#car = Car.new
+#car.turn("右")
 
-car = Car.new
-car.run(5)
+#car = Car.new
+#car.run(5)
+
+array = [1,2,3]
+puts array.class
+
+string = "Hello"
+puts string.class
