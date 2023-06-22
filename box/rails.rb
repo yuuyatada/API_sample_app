1a4383e25a3413a1771ed2bab5fa0ccdfc06477f
EC2 Default User <c134141@gmail.com>
2023-06-22 13:03:45 +0000

    railsにおけるメソッドの追加

--- box/rails.rb
+++ box/rails.rb
@@ -1,8 +1,16 @@
 class Car
+    
+    def turn(direction)
+        puts "#{direction}に曲がります。"
+    end
+    
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
-end
+  end
+  
+car = Car.new
+car.turn("右")
 
 car = Car.new
 car.run(5)
