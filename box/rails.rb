c37295f4b8142cf6cd62dfecc9c30e5a730112f6
EC2 Default User <c134141@gmail.com>
2023-06-22 13:18:13 +0000

    メソッドのレシーバーについての基礎復習１

--- box/rails.rb
+++ box/rails.rb
@@ -1,6 +1,10 @@
 # インスタンスメソッド
 class Car
-    
+     def move(direction, distance)
+      self.turn(direction)
+      self.run(distance)
+     end
+  
     def turn(direction)
        puts "#{direction}に曲がります。"
     end
@@ -11,10 +15,7 @@ class Car
 end
   
 car = Car.new
-car.turn("右")
-
-car = Car.new
-car.run(5)
+car.move("右", 5)
 
 
 #array = [1,2,3]
