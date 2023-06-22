043f23c1ced3020824b5637881d06e9f65539ffb
EC2 Default User <c134141@gmail.com>
2023-06-22 13:14:49 +0000

    クラスメソッドのやり方についての基礎

--- box/rails.rb
+++ box/rails.rb
@@ -1,24 +1,35 @@
-#class Car
+# インスタンスメソッド
+class Car
     
-   # def turn(direction)
-   #     puts "#{direction}に曲がります。"
-   # end
+    def turn(direction)
+       puts "#{direction}に曲がります。"
+    end
     
-  #def run(distance)
-  #  puts "車で#{distance}キロ走ります。"
- # end
-  #end
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
   
-#car = Car.new
-#car.turn("右")
+car = Car.new
+car.turn("右")
 
-#car = Car.new
-#car.run(5)
+car = Car.new
+car.run(5)
 
-array = [1,2,3]
-puts array.class
-puts array.join("*")
 
-string = "Hello"
-puts string.class
-puts string.join("*")
+#array = [1,2,3]
+#puts array.class
+#puts array.join("*")
+
+#string = "Hello"
+#puts string.class
+#puts string.join("*")
+
+#クラスメソッド
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
