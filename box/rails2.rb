8a14a12b2764a6317d2caf8cb2f629e94fffaf0e
EC2 Default User <c134141@gmail.com>
2023-06-25 10:51:45 +0000

    継承の確認問題

--- box/rails2.rb
+++ box/rails2.rb
@@ -1,11 +1,23 @@
+#class Car
+ # def run(distance)
+ #   puts "車で#{distance}キロ走ります。"
+ # end
+#end
+
+#class Bus < Car
+#end
+
+#bus = Bus.new
+#bus.run(5)
+
 class Car
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
 
-class Bus < Car
+class Truck < Car
 end
 
-bus = Bus.new
-bus.run(5)
+truck = Truck.new
+truck.run(5)
