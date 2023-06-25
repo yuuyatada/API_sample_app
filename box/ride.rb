978b9c36cf2300742f6a059fcef5071d39515eb3
EC2 Default User <c134141@gmail.com>
2023-06-25 10:54:17 +0000

    メソッドが２種類あるものに両方runメソッドが定義されている場合

---
+++ box/ride.rb
@@ -0,0 +1,14 @@
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+class Bus < Car
+  def run(distance)
+    puts "バスで#{distance}キロ走ります。"
+  end
+end
+
+bus = Bus.new
+bus.run(5)
